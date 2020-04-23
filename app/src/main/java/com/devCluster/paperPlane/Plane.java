5752a5e37958dc6ffe13a924e65f8c457802d1c4
Niforances <sungyun1126@gmail.com>
2020-04-23 15:22:15 +0900

    Initial Commit

---
+++ app/src/main/java/com/devCluster/paperPlane/Plane.java
@@ -0,0 +1,16 @@
+package com.devCluster.paperPlane;
+
+public class Plane {
+
+    public String title;
+    public String subTitle;
+
+    public String mainText;
+
+    public Plane(String title, String subTitle, String mainText){
+        this.title = title;
+        this.subTitle = subTitle;
+        this.mainText = mainText;
+    }
+
+}
